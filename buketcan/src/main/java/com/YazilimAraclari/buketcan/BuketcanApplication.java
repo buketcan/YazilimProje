6f572c9db3bc73e3ce53bc1d977e582ddb4327c2
bprlab <bprlab@10.200.144.19>
2019-06-18 16:27:30 +0300

    yazilimprojesi

---
+++ buketcan/src/main/java/com/YazilimAraclari/buketcan/BuketcanApplication.java
@@ -0,0 +1,13 @@
+package com.YazilimAraclari.buketcan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuketcanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuketcanApplication.class, args);
+	}
+
+}
